06d4aa56cf99f74321f4106602978d4268112350
juarisco <juarisco@outlook.com>
2018-01-17 11:17:20 -0500

    orderBy, take, get

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -9,4 +9,24 @@
         </div>
     </div>
 
+    <div class="container">
+		<h1 class="page-header">
+			Últimos usuarios registrados
+		</h1>
+		<table class="table table-hover table-striped">
+			<thead>
+				<tr>
+					<th>ID</th>
+					<th>Nombre</th>
+					<th>Email</th>
+					<th>Genero</th>
+					<th>Biografía</th>
+				</tr>
+			</thead>
+			<tbody>
+				@include('partials.list-users')
+			</tbody>
+		</table>
+	</div>
+
 @endsection
